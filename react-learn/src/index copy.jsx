02e234f5cfe1f1c617b96f2e862c4670a7d4dc58
lebRonFeng <18292996148@163.com>
2024-08-09 10:57:23 +0800

    图片定时切换

--- react-learn/src/index copy.jsx
+++ react-learn/src/index copy.jsx
@@ -1,6 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// const h1 = (<h1>Hello World <span>span元素1</span></h1>);
-// ReactDOM.render(h1, document.getElementById('root'))
-const img = (<img alt="" src="https://n.sinaimg.cn/sinakd20201217ac/213/w2048h1365/20201217/50db-kfnaptt5979574.jpg"/>)
-ReactDOM.render(img, document.getElementById('root'))
+import src1 from './assets/1.jpg'
+import src2 from './assets/2.jpg'
+import src3 from './assets/3.jpg'
+
+const srcs = [src1, src2, src3]
+
+let index = 0; //显示图片索引
+
+const container = document.getElementById('root')
+let timer = null;
+
+/**
+ * 根据index的值，显示某张图片
+ */
+function render() {
+  ReactDOM.render(<img alt="" src={srcs[index]} />, container)
+}
+
+/**
+ * 启动计时器，每隔一段时间，切换图片
+ */
+function start() {
+  stop()
+  timer = setInterval(() => {
+    index = (index + 1) % 3 //改变index
+    render()
+  }, 2000)
+}
+
+/**
+ * 停止计时器
+ */
+function stop(){
+  clearInterval(timer)
+}
+
+render()
+
+start()
+
+container.onmouseenter = function(){
+  stop();
+}
+
+container.onmouseleave = function(){
+  start();
+}
--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import src1 from './assets/1.jpg'
+import src2 from './assets/2.jpg'
+import src3 from './assets/3.jpg'
 
-let num = 0;
-setInterval(() => {
-  num++;
-  const div = (
-    <div title="abc"> 
-      {num}
-    </div> 
-  )
-  ReactDOM.render(div, document.getElementById('root'))
-},1000)
+let index = 0; //索引
+const srcs = [src1, src2, src3]
+const container = document.getElementById('root')
+let timer = null
 
+function render(){
+  ReactDOM.render(<img src={srcs[index]} alt="" />, container)
+}
+
+function start() {
+  stop()
+  timer = setInterval(() => {
+    index = (index + 1) % 3
+    render()
+  }, 2000)
+}
+
+function stop(){
+  clearInterval(timer)
+}
+
+render()
+start()
+
+container.onmouseenter = function(){
+  stop()
+}
+
+container.onmouseleave = function(){
+  start()
+}
 
