01d9df76165e6e10ce0b0c44f853a00f7c732414
lebRonFeng <18292996148@163.com>
2024-08-15 10:08:06 +0800

    学生列表demo

--- react-learn/src/index copy.jsx
+++ react-learn/src/index copy.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from './components/StudentList'
+import Student from './components/Student'
 
 // 获取所有学生数据
-function fetchAllStudents() {
-  const stus = [
+function fetchStudents() {
+  // var stus = await fetch('https://shanhe.kim/api/za/shenhuifu.php')
+  //   .then(resp => resp.json()).then(resp => resp.data)
+  var stus = [
     { address: '新疆乌鲁木齐', appkey: 'demo13_15450988', birth: 1995, ctime: 1554049415, email: '382246268@qq.com', id: 27414, name: '张楠', phone: 18897896655, sNo: '11011', sex: 1, utime: 1561472121 },
     { address: '山东青岛', appkey: 'demo13_15450988', birth: 1995, ctime: 1554049417, email: '382246268@qq.com', id: 27415, name: '李飞', phone: 18897896655, sNo: '11012', sex: 0, utime: 1561472121 },
     { address: '黑龙江哈尔滨市XXX', appkey: 'demo13_15450988', birth: 1995, ctime: 1554049415, email: '382246268@qq.com', id: 27416, name: '洪湖就', phone: 18897896655, sNo: '11013', sex: 1, utime: 1561472121 },
   ]
+  console.log(stus)
   return stus
 }
 
-async function render() {
-  ReactDOM.render("正在加载中...", document.getElementById('root'))
-  const stus = fetchAllStudents(); //获取学生数组
-  ReactDOM.render(<StudentList stus={stus}/>, document.getElementById('root'))
+fetchStudents()
+
+const s = {
+  address: '新疆乌鲁木齐',
+  appkey: 'demo13_15450988',
+  birth: 1995, ctime: 1554049415,
+  email: '382246268@qq.com', 
+  id: 27414,
+  name: '张楠', 
+  phone: 18897896655,
+  sNo: '11011', 
+  sex: 1, 
+  utime: 1561472121
 }
 
-render()
-
+ReactDOM.render(<Student {...s}/>, document.getElementById('root'))
