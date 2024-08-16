3cc5f3c41a00bd12ba5a59469755f2838637d2ea
lebRonFeng <18292996148@163.com>
2024-08-16 16:33:25 +0800

    分页组件

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TickControl from './TickControl';
+import PagerTest from './components/PagerTest'
 
-// const btn = <button onClick={() => {console.log('点击了！！！')}} onMouseEnter={() => {
-//   console.log('鼠标移入了')
-// }}>点击我</button>
-const tc = new TickControl()
-console.log(tc)
-ReactDOM.render(<TickControl/>, document.getElementById('root'))
+ReactDOM.render(
+  <PagerTest />,
+  document.getElementById('root')) 
