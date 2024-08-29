cadd097789093d1ee231b0fd10eb77f6caee3618
lebRonFeng <18292996148@163.com>
2024-08-29 09:41:56 +0800

    生命周期新版

--- react-learn/src/App copy 2.js
+++ react-learn/src/App copy 2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import NewLifeCycle from './NewLifeCycle'
+import Test from './Test'
 
 export default class App extends Component {
 
@@ -10,7 +10,7 @@ export default class App extends Component {
   render(){
     return (
       <div>
-       <NewLifeCycle n={this.state.number}/>
+       <Test n={this.state.number}/>
        <p>
           <button onClick={() => {
             this.setState({
