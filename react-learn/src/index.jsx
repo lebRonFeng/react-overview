e1377e720362f196840508a6ba470bda2301149e
lebRonFeng <18292996148@163.com>
2024-08-29 18:16:53 +0800

    三栏布局

--- react-learn/src/index.jsx
+++ react-learn/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './components/Test'
+import App from './App'
 
 ReactDOM.render(
   <div>
-    <Test/>
+    <App/>
   </div>,
   document.getElementById('root'))
 
